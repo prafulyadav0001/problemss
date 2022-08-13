80a2cdca6fbe1193785ebc05444310b88819a405
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2022-08-13 22:14:13 +0530

    Add files via upload

---
+++ maxsub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class maxsub {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        for (int i=0;i<t;i++){
+            int n=in.nextInt();
+            System.out.println(n*2);
+        }
+    }
+}
